3915d3c4f814e94b4e759aeea5a8d5f741824683
Okiushi <okiushi.ldb.private@gmail.com>
2023-03-27 12:46:24 +0200

    First vers. of view and more

--- app/model.py
+++ app/model.py
@@ -1,37 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import os
-import sys
+import pickle
 
+# export des données dans le fichier users.pickl
+def export_users(data):
+    with open("users.pickle", "wb") as file:
+        pickle.dump(data, file)
 
-
-
+# import des données depuis le fichier users.pickle
+def import_users():
+    with open("users.pickle", "rb") as file:
+        return pickle.load(file)
 
 class User():
     def __init__(self, user_name, first_name, last_name, password, role=[]):
         self.id = 0
-        self.__user_name = user_name
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__password = password
-        self.__suspended = False
-        self.__role = []
+        self.user_name = user_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.suspended = False
+        self.role = []
 
     def get(self, var):
         if var =="user_name":
-            return self.__user_name
+            return self.user_name
         elif var =="first_name":
-            return self.__first_name
+            return self.first_name
         elif var =="last_name":
-            return self.__last_name
+            return self.last_name
         elif var =="suspended":
-            return self.__suspended
+            return self.suspended
         elif var =="role":
-            return self.__role
-        else: print("quelle info voulez-vous ?")
+            return self.role
+        
+def get_user(id):
+    return import_users()[str(id)]
 
+def create_user(user_id,user_name,first_name,last_name,password):
+        import_users().update({f"{user_id}":User(user_name,first_name,last_name,password)})
 
-jhon = User("jdoe", "Jhon", "Doe", "123456")
-
-print(jhon.get("user_name"))
+def delete_user(user_id):
+    import_users().pop(f"{user_id}")
--- app/view.py
+++ app/view.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import os
-import sys
-import json
+import controller
+
+def start_menu():
+    print("Bienvenue sur le gestionaire d'utilisateurs")
+    print("1. Créer un utilisateur")
+    print("2. Obtenire une information sur un utilisateur")
+    print("3. Supprimer un utilisateur")
+    user_input = input("Que voulez-vous faire ? ")
+    if user_input == "1":
+        controller.model.create_user()
+    if user_input == "2":
+        a = controller.model.get_user(101)
+    if user_input == "3":
+        controller.model.delete_user()
+
+start_menu()
