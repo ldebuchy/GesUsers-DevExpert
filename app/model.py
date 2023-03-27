1260776b0f0daa1c5d08f221f5073e41c99f58b2
Marius-Pelletier <marius.clement.pelletier@gmail.com>
2023-03-27 11:37:15 +0200

    creation gate user

--- app/model.py
+++ app/model.py
@@ -3,42 +3,37 @@
 
 import os
 import sys
-import pickle
 
 
 
-# Création de la classe User
+
+
 class User():
-    def __init__(self, user_name, first_name, last_name, password, role=[]):
+    def init(self, user_name, first_name, last_name, password, role=[]):
         self.id = 0
-        self.user_name = user_name
-        self.first_name = first_name
-        self.last_name = last_name
-        self.password = password
-        self.suspended = False
-        self.role = []
+        self.__user_name = user_name
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__password = password
+        self.__suspended = False
+        self.__role = []
+        
+    def get_info(self, var):
+        if var =="user_name":
+            return self.__user_name
+        elif var =="first_name":
+            return self.__first_name
+        elif var =="last_name":
+            return self.__last_name
+        elif var =="password":
+            return self.__password
+        elif var =="suspended":
+            return self.__suspended
+        elif var =="role":
+            return self.__role
+        else: print("quelle info voulez-vous ?")
 
-user_list = {}
 
-# Création de deux utilisateurs
 jhon = User("jdoe", "Jhon", "Doe", "123456")
-mike = User("msirus", "Mike", "Sirus", "123456")
 
-# Ajout des utilisateurs à la liste
-user_list.append(jhon)
-user_list.append(mike)
-user_list = {"100": jhon,"101": mike}
-
-# export des données dans le fichier users.pickle
-with open("users.pickle", "wb") as file:
-    pickle.dump(user_list, file)
-
-# import des données depuis le fichier users.pickle
-with open("users.pickle", "rb") as file:
-    user_list = pickle.load(file)
-
-# Affichage des données
-try:
-    print(user_list["101"].last_name)
-except:
-    print("User not found")
+print(jhon)
