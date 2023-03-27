523d575243e0c57a60df4ad88a3ae1bdfba3ae79
roro-zer <108422754+roro-zer@users.noreply.github.com>
2023-03-27 12:27:34 +0200

    creation et suppression utilisateur

--- app/model.py
+++ app/model.py
@@ -9,29 +9,30 @@ import sys
 
 
 class User():
-    def __init__(self, user_name, first_name, last_name, password, role=[]):
+    def init(self, user_name, first_name, last_name, password, role=[]):
         self.id = 0
-        self.__user_name = user_name
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__password = password
-        self.__suspended = False
-        self.__role = []
+        self.user_name = user_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.suspended = False
+        self.role = []
+    # create a user 
 
-    def get(self, var):
-        if var =="user_name":
-            return self.__user_name
-        elif var =="first_name":
-            return self.__first_name
-        elif var =="last_name":
-            return self.__last_name
-        elif var =="suspended":
-            return self.__suspended
-        elif var =="role":
-            return self.__role
-        else: print("quelle info voulez-vous ?")
 
+user_list={}
+    # create a user 
+def create_user(user_id,user_name,first_name,last_name,password):
+        user_list.update({f"{user_id}":User(user_name,first_name,last_name,password)})
+
+def delete_user(user_id):
+    user_list.pop(f"{user_id}")
+
+
+
+user_list = []
 
 jhon = User("jdoe", "Jhon", "Doe", "123456")
 
-print(jhon.get("user_name"))
+user_list.append(jhon)
+
