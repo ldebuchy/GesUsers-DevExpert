96bb83935c95b5db8523e25d4d24d14e848d1344
Okiushi <okiushi.ldb.private@gmail.com>
2023-03-27 09:50:39 +0200

    User object

--- app/model.py
+++ app/model.py
@@ -3,4 +3,27 @@
 
 import os
 import sys
-import json
+
+
+
+
+
+class User():
+    def init(self, user_name, first_name, last_name, password, role=[]):
+        self.id = 0
+        self.user_name = user_name
+        self.first_name = ""
+        self.last_name = ""
+        self.password = password
+        self.suspended = False
+        self.role = []
+
+
+
+
+user_list = []
+
+jhon = User("jdoe", "Jhon", "Doe", "123456")
+
+user_list.append(jhon)
+
